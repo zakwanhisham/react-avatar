e3e0a3e91a54d9e462377eff4a90ca80158a3f98
Zakwan <hishamzakwan@gmail.com>
2023-01-16 15:48:18 +0800

    clean up the code

--- src/Wrapper.test.tsx
+++ src/Wrapper.test.tsx
@@ -1,10 +1,13 @@
 import { describe, expect, it, beforeEach } from "vitest";
-import { render, screen, cleanup, fireEvent } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 
 import Avatar from "./Avatar";
 import Wrapper from "./Wrapper";
 
 describe("Wrapper", () => {
+  beforeEach(() => {
+    cleanup();
+  });
   it("should render a caption and an image", () => {
     const url = "https://catass.com/cat/says/hello%20world!";
     const alt = "@github-handle";
